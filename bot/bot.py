5aca0c248b156dd78b4dcb44b9f41981c6d95bbf
Arsenka <106221641+Wvopper@users.noreply.github.com>
2023-01-05 14:00:57 +0300

    added first settings of add_new_book button (not connected with google_sheets)

--- bot/bot.py
+++ bot/bot.py
@@ -8,22 +8,93 @@ bot = telebot.TeleBot('5862500583:AAFLMRZNlGi6aZ28c1LqmV9xpMG5mSmLQ9Q')
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f"Привет, <b>{message.from_user.first_name}</b>. \n Этот бот предназначен для подготовки к сочинениям по русскому языку и литературе. \n С его помощью вы можете найти краткие пересказы на интересующие вас произведения, вспомнить главных героев, а также посмотреть, какие проблемы расскрываются. \n  Также вы можете добавить прочитанное вами произведение"
-    markup = ttp.InlineKeyboardMarkup(row_width=2)
-    button1 = ttp.InlineKeyboardButton(text='Поиск произведения', callback_data='btn1')
-    button2 = ttp.InlineKeyboardButton(text='Добавить произведение', callback_data='btn2')
+    markup = ttp.ReplyKeyboardMarkup(row_width=2)
+    button1 = ttp.KeyboardButton(text='Поиск произведения')
+    button2 = ttp.KeyboardButton(text='Добавить произведение')
     markup.add(button1, button2)
 
     bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
 
 
-@bot.callback_query_handler(func= lambda callback: callback.data)
-def callback_checking(callback):
-    if callback.data == 'btn1':
-        markup = ttp.InlineKeyboardMarkup(row_width=2)
-        button1 = ttp.InlineKeyboardButton(text='Поиск по автору', callback_data='auth_search')
-        button2 = ttp.InlineKeyboardButton(text='Поиск по названию', callback_data='name_search')
-        markup.add(button1, button2)
-        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберете вид поиска:', reply_markup=markup)
+@bot.message_handler(content_types=['text'])
+def book(message):
+    markup = ttp.ReplyKeyboardMarkup(row_width=2)
+    backbutton = ttp.KeyboardButton(text='Вернуться назад')
+    markup.add(backbutton)
+    if message.text == 'Добавить произведение':
+        mess1 = "Для того, чтобы добавить новое произведение вы должны знать формат заполнения. Он содержит: \n 1) Автор (пример: А.С.Пушкин) \n 2) Жанр произведения (роман\проза\стихотоворение\...) \n 3) Название (примеры: Капитанская доска\Дубровский\...) \n 4) Главные герои (пример: Пётр Гринёв, Мария, ... ) \n 5) Проблемы произведения (нравственный выбор," \
+                "подвиг во имя любви, ...) \n 5) Ссылка на краткий пересказ (желательно, чтобы это были источники briefly.ru\litrekon.ru\obrazovaka.ru) \n Продолжить (напишите <b>Да</b>)? "
+        sent = bot.send_message(message.chat.id, mess1, reply_markup=markup, parse_mode='html')
+        if message.text == 'Добавить произведение':
+            bot.register_next_step_handler(sent, add_author)
+        elif message.text == 'Вернуться назад':
+            bot.register_next_step_handler(sent, start)
+    elif message.text == 'Поиск произведения':
+        pass
+
+
+new_book = []
+
+
+def add_author(message):
+    markup = ttp.ReplyKeyboardMarkup(row_width=2)
+    backbutton = ttp.KeyboardButton(text='Вернуться назад')
+    markup.add(backbutton)
+    sent = bot.send_message(message.chat.id, 'Введите автора произведения:', reply_markup=markup)
+    new_book.append(message.text)
+    print(*new_book)
+    bot.register_next_step_handler(sent, add_genre)
+
+
+def add_genre(message):
+    markup = ttp.ReplyKeyboardMarkup(row_width=2)
+    backbutton = ttp.KeyboardButton(text='Вернуться назад')
+    markup.add(backbutton)
+    sent = bot.send_message(message.chat.id, 'Введите жанр произведения:', reply_markup=markup)
+    new_book.append(message.text)
+    print(*new_book)
+    bot.register_next_step_handler(sent, add_name)
+
+
+def add_name(message):
+    markup = ttp.ReplyKeyboardMarkup(row_width=2)
+    backbutton = ttp.KeyboardButton(text='Вернуться назад')
+    markup.add(backbutton)
+    sent = bot.send_message(message.chat.id, 'Введите название произведения:', reply_markup=markup)
+    new_book.append(message.text)
+    print(*new_book)
+    bot.register_next_step_handler(sent, add_heroes)
+
+
+def add_heroes(message):
+    markup = ttp.ReplyKeyboardMarkup(row_width=2)
+    backbutton = ttp.KeyboardButton(text='Вернуться назад')
+    markup.add(backbutton)
+    sent = bot.send_message(message.chat.id, 'Введите главных героев произведения через запятую:', reply_markup=markup)
+    new_book.append(message.text)
+    print(*new_book)
+    bot.register_next_step_handler(sent, add_problems)
+
+
+def add_problems(message):
+    markup = ttp.ReplyKeyboardMarkup(row_width=2)
+    backbutton = ttp.KeyboardButton(text='Вернуться назад')
+    markup.add(backbutton)
+    sent = bot.send_message(message.chat.id, 'Введите проблемы раскрытые в произведении через запятую:', reply_markup=markup)
+    new_book.append(message.text)
+    print(*new_book)
+    bot.register_next_step_handler(sent, add_url)
+
+
+def add_url(message):
+    markup = ttp.ReplyKeyboardMarkup(row_width=2)
+    backbutton = ttp.KeyboardButton(text='Вернуться назад')
+    markup.add(backbutton)
+    sent = bot.send_message(message.chat.id, 'Введите ссылку на краткий пересказ (желательно, чтобы это были сайты litrekon.ru, briefly.ru, obrazovaka.ru):', reply_markup=markup)
+    new_book.append(message.text)
+    new_book.pop(0)
+    print(*new_book)
+    bot.register_next_step_handler(sent, start)
 
 
 # bot polling
